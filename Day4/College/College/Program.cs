55c376e23a513720e5d6d42c093218dea0e6cc02
mridula-narang <narangmg@gmail.com>
2024-09-09 14:37:48 +0530

    Day 4 assignment

---
+++ Day4/College/College/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace College
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1 = new Student(101,"Ramu","12-July-2001", "9876543210","ramu@gmail.com");
+            Student std2 = new Student(102, "Harry", "31-July-1975", "8584986749", "harry@gmail.com");
+            std1.DisplayId();
+            std2.DisplayId();
+        }
+    }
+}
