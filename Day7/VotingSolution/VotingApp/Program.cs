30d7e3715d4ba9a806bc29b9052a65dfa78f2235
mridula-narang <narangmg@gmail.com>
2024-09-12 12:02:24 +0530

    Learning and working with interfaces and abstract classes

---
+++ Day7/VotingSolution/VotingApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VotingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Day7/VotingSolution/VotingApp/ValidateCustomer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotingApp
+{
+    internal class ValidateCustomer : IValidateCustomer
+    {
+        public int CalculateAge(DateTime dateOfBirth)
+        {
+            var customerAge = DateTime.Now.Year - dateOfBirth.Year;
+            if (DateTime.Now < dateOfBirth.AddYears(customerAge))
+            {
+                customerAge--;
+            }
+            return customerAge;
+        }
+        public void ValidateCustomerByAge(Customer customer)
+        {
+            var age = CalculateAge(customer.DateOfBirth);
+            PrintResult(customer, age);
+        }
+
+        private void PrintResult(Customer customer, int age)
+        {
+            var salutation = customer.Gender == "M" ? "Mr" : "Ms";
+            if (age < 18)
+            {
+                Console.WriteLine($"Dear {salutation}. {customer.Name} you are {age} years old and so not eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine($"Dear {salutation}. {customer.Name} you are {age} years old and so you are eligible to vote.");
+            }
+        }
+    }
+}
