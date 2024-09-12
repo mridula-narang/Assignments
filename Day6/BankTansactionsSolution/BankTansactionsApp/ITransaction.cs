971b3cc7a2423781a72ee77ecde197b9be0e75c8
mridula-narang <narangmg@gmail.com>
2024-09-12 11:44:59 +0530

    Day 6 assignments. Product management system and Banking application

---
+++ Day6/BankTansactionsSolution/BankTansactionsApp/ITransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTansactionsApp
+{
+    internal interface ITransaction
+    {
+        public void Deposit(double Amount);
+        public void Withdraw(double Amount);
+        public double GetBalance();
+    }
+}
