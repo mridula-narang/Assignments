94f888e013b2aca547d8f81af4327208862c3920
mridula-narang <narangmg@gmail.com>
2024-09-13 15:22:25 +0530

    Day 8 - Assignment 1

---
+++ Day8/Assignment/EmployeePromotionSolution/EmployeePromotionApp/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePromotionApp
+{
+    public class Employee
+    {
+        int id, age;
+        string name;
+        double salary;
+
+        public Employee()
+        {
+        }
+
+        public Employee(int id, int age, string name, double salary)
+        {
+            this.id = id;
+            this.age = age;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void TakeEmployeeDetailsFromUser()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Please enter the employee ID");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Please enter the employee name");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            name = Console.ReadLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Please enter the employee age");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Please enter the employee salary");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            salary = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return "Employee ID : " + id + "\nName : " + name + "\nAge : " + age + "\nSalary : " + salary;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Age { get => age; set => age = value; }
+        public string Name { get => name; set => name = value; }
+        public double Salary { get => salary; set => salary = value; }
+
+    }
+}
---
+++ Day8/Assignment/EmployeePromotionSolution/EmployeePromotionApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeePromotionApp
+{
+    internal class Program
+    {
+        EmployeePromotion employeePromotion = new EmployeePromotion();
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.employeePromotion.InputEmployeeDetails();
+            //program.employeePromotion.DisplayPromotionOrder(); //method to be called for easy question 1
+            //program.employeePromotion.FindEmployeePosition(); //method to be called for easy question 2
+            //program.employeePromotion.RemoveSpace(); //method to be called for easy question 3
+            program.employeePromotion.SortEmployeeNames(); //method to be called for easy question 4
+            
+        }
+    }
+}
