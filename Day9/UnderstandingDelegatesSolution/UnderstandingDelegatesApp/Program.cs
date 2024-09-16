b9fe998dadd55459a9f0c2472d937feab94563a2
mridula-narang <narangmg@gmail.com>
2024-09-16 14:55:19 +0530

    Understanding LINQ

--- Day9/UnderstandingDelegatesSolution/UnderstandingDelegatesApp/Program.cs
+++ Day9/UnderstandingDelegatesSolution/UnderstandingDelegatesApp/Program.cs
@@ -4,47 +4,40 @@ namespace UnderstandingDelegateApp
 {
     internal class Program
     {
-        //public delegate void CalculateDelegate(int n1, int n2);//creating out delegate type
-
+        class Employee
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public override string ToString()
+            {
+                return "Id = " + ID + ", Name = " + Name + ", Salary = " + Salary;
+            }
+        }
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee { ID = 101, Name = "Mark", Salary = 5000 },
+            new Employee { ID = 102, Name = "John", Salary = 14000 },
+            new Employee { ID = 103, Name = "Smith", Salary = 5500 },
+            new Employee { ID = 104, Name = "Watson", Salary = 15000 }
+        };
         public Program()
         {
-            // CalculateDelegate calculateDelegate = new CalculateDelegate(Multiply);
-            //Action<int, int> calculateDelegate = Multiply;
-            Func<int, int, int> calculateDelegate = Multiply;
-            calculateDelegate += Add;//Multicast Delegate
-            calculateDelegate -= Multiply;
-            Calculate(calculateDelegate, 10, 20);
+            UnderstandingLINQ();
         }
-        //public void Add(int num1,int num2)
-        //{
-        //    int result = num1 + num2;
-        //    Console.WriteLine($"The sum of {num1} and {num2} is {result}");
-        //}
-        //public void Multiply(int num1, int num2)
-        //{
-        //    int result = num1 * num2;
-        //    Console.WriteLine($"The product of {num1} and {num2} is {result}");
-        //}
 
+        private void UnderstandingLINQ()
+        {
+            //var highPaidEmployees = employees.Where(emp => emp.Salary > 10000 && emp.Name.Contains("oh"));
+            //var highPaidEmployees = employees.Where(e => e.Name.Contains("o")).OrderBy(emp => emp.Salary);
+            //var highPaidEmployees = employees.Where(e => e.Name.Contains("o")).OrderByDescending(emp => emp.Salary);
+            var highPaidEmployees = employees.Where(e => e.Name.Contains("o")).OrderByDescending(emp => emp.Salary).Take(1); // Take(1) will return only one record. if we use Take(2) then it will return 2 records.
+            foreach (var employee in highPaidEmployees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
 
-        public int Add(int num1, int num2)
-        {
-            int result = num1 + num2;
-            return result;
-        }
-        public int Multiply(int num1, int num2)
-        {
-            int result = num1 * num2;
-            return result;
-        }
-        //-----------------------------------------------------------------------------
-        //public void Calculate(Action<int,int> myDelegate,int n1, int n2)
-        public void Calculate(Func<int, int, int> myDelegate, int n1, int n2)
-        {
-            int result = myDelegate(n1, n2);
-            Console.WriteLine($"The sum of {n1} and {n2} is {result}");
-
-        }
         static void Main(string[] args)
         {
             new Program();
--- Day9/UnderstandingDelegatesSolution/UnderstandingDelegatesApp/obj/Debug/net8.0/UnderstandingDelegatesApp.AssemblyInfo.cs
+++ Day9/UnderstandingDelegatesSolution/UnderstandingDelegatesApp/obj/Debug/net8.0/UnderstandingDelegatesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnderstandingDelegatesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d6fe93c62bbb341e3915ab2b9eed0ff82097bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnderstandingDelegatesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnderstandingDelegatesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
