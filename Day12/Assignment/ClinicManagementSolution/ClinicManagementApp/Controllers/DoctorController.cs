f016d78d317298268e0b3b6882d13f68e3551def
mridula-narang <narangmg@gmail.com>
2024-09-23 15:35:21 +0530

    Day 12 Assignment files

---
+++ Day12/Assignment/ClinicManagementSolution/ClinicManagementApp/Controllers/DoctorController.cs
@@ -0,0 +1,19 @@
+﻿using ClinicManagementApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManagementApp.Controllers
+{
+    public class DoctorController : Controller
+    {
+        List<Doctor> doctors = new List<Doctor>
+        {
+            new Doctor { Id = 1, Name = "Dr. Theodore Altman", PhoneNumber = "7795785774", Department = "Cardiology", Specialization = "Cardiothoracic Surgeon", Image = "/images/1.jpg" },
+            new Doctor { Id = 2, Name = "Dr. Meredith Grey", PhoneNumber = "8867587848", Department = "Neurology", Specialization = "Neuro-oncologist" ,Image = "/images/2.jpg"},
+            new Doctor { Id = 3, Name = "Dr. Miranda Bailey", PhoneNumber = "8869577677", Department = "Ophthalmology", Specialization = "Retina", Image = "/images/3.jpg" },
+        };
+        public IActionResult Index()
+        {
+            return View(doctors);
+        }
+    }
+}
---
+++ Day12/Assignment/ClinicManagementSolution/ClinicManagementApp/Models/Doctor.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicManagementApp.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Department { get; set; }
+        public string? Specialization { get; set; }
+        public string? Image { get; set; }
+    }
+}
