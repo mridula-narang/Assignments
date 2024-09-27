3bf8640be87726cb97af35ccbac1fa9450779206
mridula-narang <narangmg@gmail.com>
2024-09-27 10:18:19 +0530

    Day 13

---
+++ Day13/ShoppingSolution/ShoppingApplication/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingApplication.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public string? Image { get; set; }
+    }
+}
